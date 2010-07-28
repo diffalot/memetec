38204038236e5a10ef6ae07ae7408845d07e715b
papyromancer <papyromancer@papyromancer.net>
2010-07-28 18:35:46 +0000

    added index.html

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-var http = require('http'),
-        port = process.env.PORT || 8001
+var sys = require("sys"),
+    http = require("http"),  
+    url = require("url"),  
+    path = require("path"),  
+    fs = require("fs"),
+    
+    Mu = require('./lib/Mu/lib/mu');
 
-        http.createServer(function (req, res) {
-              res.writeHead(200, {'Content-Type': 'text/plain'})
-                res.end('Hello World\n')
-                }).listen(parseInt(port));
+Mu.templateRoot = './views';
 
-console.log('Server running at http://127.0.0.1:8124/');
+http.createServer(function(request, response) {  
+    var uri = url.parse(request.url).pathname;  
+    var filename = path.join(process.cwd(), uri);  
+    path.exists(filename, function(exists) {  
+        if(!exists) {  
+            response.sendHeader(404, {"Content-Type": "text/plain"});  
+            response.write("404 Not Found\n");  
+            response.close();  
+            return;  
+        }  
+
+        fs.readFile(filename, "binary", function(err, file) {  
+            if(err) {  
+                response.sendHeader(500, {"Content-Type": "text/plain"});  
+                response.write(err + "\n");  
+                response.close();  
+                return;  
+            }  
+
+            response.sendHeader(200);  
+            response.write(file, "binary");  
+            response.close();  
+        });  
+    });  
+}).listen(8000);  
+  
+sys.puts("Server running at http://localhost:8000/");
