8eb449e5ffa7b5be82f578eff4455c27cffd4980
papyromancer <papyromancer@papyromancer.net>
2010-08-30 08:06:21 -0500

    adding in the dreaded vrequire

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require.paths.unshift('./vendor')
-
-var app = require('express').createServer();
+vrequire = function(lib) { 
+    require.paths.unshift("vendor/.npm/" + lib + "/active/package/lib");
+    return require(lib); 
+}
+var app = vrequire('express').createServer();
 app.get('/', function(req, res){
-        res.send('Hello World');
-        });
+    res.send('Hello World');
+    });
 app.listen(3000);
 var sys = require('sys');
 sys.puts("Server running at http://localhost:3000/");
