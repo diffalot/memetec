760743996ae4aa17b07836c717450866d74f591a
papyromancer <papyromancer@papyromancer.net>
2010-08-30 07:46:23 -0500

    trying out a package.json that requires express and an express hello world

--- server.js
+++ server.js
@@ -1,37 +1,9 @@
-var sys = require("sys"),
-    http = require("http"),  
-    url = require("url"),  
-    path = require("path"),  
-    fs = require("fs"),
-    
-    Mu = require('./lib/Mu/lib/mu');
 
-Mu.templateRoot = './views';
 
-http.createServer(function(request, response) {  
-    var uri = url.parse(request.url).pathname;  
-    var filename = path.join(process.cwd(), uri);  
-    path.exists(filename, function(exists) {  
-        if(!exists) {  
-            response.sendHeader(404, {"Content-Type": "text/plain"});  
-            response.write("404 Not Found\n");  
-            response.close();  
-            return;  
-        }  
-
-        fs.readFile(filename, "binary", function(err, file) {  
-            if(err) {  
-                response.sendHeader(500, {"Content-Type": "text/plain"});  
-                response.write(err + "\n");  
-                response.close();  
-                return;  
-            }  
-
-            response.sendHeader(200);  
-            response.write(file, "binary");  
-            response.close();  
-        });  
-    });  
-}).listen(8000);  
-  
-sys.puts("Server running at http://localhost:8000/");
+var app = require('express').createServer();
+app.get('/', function(req, res){
+        res.send('Hello World');
+        });
+app.listen(3000);
+var sys = require('sys');
+sys.puts("Server running at http://localhost:3000/");
