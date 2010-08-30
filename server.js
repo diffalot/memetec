d26306f61efad16a0c59cdcbd9f575be555b05a9
papyromancer <papyromancer@papyromancer.net>
2010-08-30 08:22:14 -0500

    * removing the vrequire * tidying up theserver.js file for readability

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-//require.paths.unshift('./vendor')
-vrequire = function(lib) { 
-    require.paths.unshift("npm/" + lib + "/active/package/lib");
-    return require(lib); 
-}
-var app = vrequire('express').createServer();
+var app = require('express').createServer();
+
 app.get('/', function(req, res){
     res.send('Hello World');
     });
+
 app.listen(3000);
+
+
 var sys = require('sys');
 sys.puts("Server running at http://localhost:3000/");
