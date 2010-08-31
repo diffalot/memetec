edf600caa7d10a4ad9dbfe92a4fb2b2d8667f9a2
papyromancer <papyromancer@papyromancer.net>
2010-08-31 01:24:22 -0500

    put in place a simple way to read dev environment settings from development-settings.json

--- server.js
+++ server.js
@@ -23,29 +23,61 @@
 
 require.paths.unshift('./npm')
 var sys = require('sys'),
+    fs = require('fs'),
     express = require('express'),
-    oauth = require('oauth').OAuth
+    oauth = require('oauth'),
     haml = require('hamljs');
 
 
 /**
-  Are we in Development or Production Mode
-/**
+  Are we in Development or Production Modea
+**/
 
-// TODO: Setup different environments for development and production.
+var settings = undefined;
+
+if (process.env.PORT) {
+  settings.environment = "Production";
+  settings.port = process.env.PORT;
+  settings.redis_host = process.env.REDIS_HOST;
+  settings.redis_pass = process.env.REDIS_PASS;
+  settings.twitter_key = process.env.TWITTER_KEY;
+  settings.twitter_secret = process.env.TWITTER_SECRET;
+  }
+else {
+  settings = JSON.parse( fs.readFileSync('development-settings.json', encoding='utf8') );
+  }
 
 
 /**
   Setup the Redis connection for user/session management
+**/
 
 var RedisCredentials = {
-    host: process.env.REDIS_HOST,
-    pass: process.env.REDIS_PASS,
-    port: parseInt(process.env.REDIS_PORT)
+    host: settings.redis_host,
+    pass: settings.redis_pass,
+    port: parseInt(settings.redis_port)
     };
 
-var redis = require('redis').createClient(RedisCredentials.port, RedisCredentials.host);
+var redis = require('redis-client').createClient(RedisCredentials.port, RedisCredentials.host);
+
+
+/**
+  We're using twitter for authentication
 **/
+
+var Twitter = new oauth.OAuth(
+    'http://api.twitter.com/oauth/request_token', 
+    'http://api.twitter.com/oauth/access_token', 
+    settings.twitter_key, 
+    settings.twitter_secret, 
+    '1.0', 
+    null, 
+    'HMAC-SHA1'
+    );
+if (RedisCredentials.pass) {
+    redis.auth(RedisCredentials.pass);
+    }
+
 
 /**
   Setup the Express Application
@@ -60,14 +92,18 @@ app.configure(function(){
 
 /**
   Express Routing
-  /:meme  --> loads up that meme
-  /       --> the home page
+  /:meme    --> loads up that meme
+  /         --> the home page if a session has been established
+  /login    --> start Twitter oauth
+  /callback --> register a session
 **/
 
 app.get('/:meme', function(req, res){
     if (req.params.meme) {
       res.render('meme', {
-        locals: { meme: req.params.meme }
+        locals: {
+          meme: req.params.meme
+          }
         });
       }
     else {
@@ -76,8 +112,34 @@ app.get('/:meme', function(req, res){
       }
     });
 
+
+
 app.get('/', function(req, res){
-    res.render('home');
+    if (req.session['access_token']) {
+      sys.puts(JSON.stringify(req.session));
+      return Twitter.getProtectedResource(
+        'http://api.twitter.com/1/account/verify_credentials.json',
+        'GET',
+        req.session['access_token'],
+        req.session['access_secret'],
+        function(error, data, response) {
+          return res.render('home', {
+            locals: {
+              user: JSON.parse(data),
+              anon: false
+              }
+            });
+          }
+        );
+      } 
+    else {
+      return res.render('home', {
+        locals: {
+          user: "anonymous",
+          anon: true
+          }
+        });
+      }
     });
 
 
@@ -85,5 +147,5 @@ app.get('/', function(req, res){
   Let's go!
 **/
 
-app.listen(parseInt(process.env.PORT || 3000), null);
-sys.puts("Server running at http://localhost:3000/");
+app.listen(parseInt( settings.port ), null);
+sys.puts(settings.environment + " server running at http://localhost:" + settings.port + "/");
